21c9de32371fa957e739aa84ab165c487ed11024
ajinkya <ajinkya.wan@gmail.com>
2019-02-24 12:48:26 +0530

    dependencies added

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/calc-app/'));
 app.listen(process.env.PORT || 8080);
 
 //path location strategy
 
 app.get('/*', function (req,res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/calc-app/index.html'));
   
 })
 
