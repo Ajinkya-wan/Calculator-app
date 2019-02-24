b187eec12991b82ab9cd4cca364ff272dd52047e
ajinkya <ajinkya.wan@gmail.com>
2019-02-24 14:23:09 +0530

    backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  host : 'http://localhost:4002',
+  host : 'https://cal-back.herokuapp.com',
   production: false
 };
